211cc63f79d643537e96aa3c2ceff9218377c35d
furperson <56189608+furperson@users.noreply.github.com>
2020-12-30 21:13:00 +0700

    Add files via upload
    Добавлено линейное движение(не рабочее).

--- divera4988.cpp
+++ divera4988.cpp
@@ -1,6 +1,6 @@
 #include "drivera4988.h"
 
-Drivera::Drivera(int numpinstep, int numpindir, int numpinm1, int numpinm2, int numpinm3, int step, int speed, int accel)
+Drivera::Drivera(int numpinstep, int numpindir, int numpinm1, int numpinm2, int numpinm3, int step, int speed)
 {
 	_numpinstep = numpinstep;
 	_numpindir = numpindir;
@@ -9,20 +9,31 @@ Drivera::Drivera(int numpinstep, int numpindir, int numpinm1, int numpinm2, int 
 	_numpinm3 = numpinm3;
 	_speed = speed;
 	_step = step;
-	_accel = accel;
 	pinMode(numpinstep, OUTPUT);
 	pinMode(numpindir, OUTPUT);
 	pinMode(numpinm1, OUTPUT);
 	pinMode(numpinm2, OUTPUT);
 	pinMode(numpinm3, OUTPUT);
 }
+void Drivera::move(int step, bool direct){
+	digitalWrite(_numpindir, direct);
+	for(int i = 0; i < step; i++) {
+	digitalWrite(_numpinstep, HIGH);
+	delay(_speed);
+	digitalWrite(_numpinstep, LOW);
+	delay(_speed);
+	}
+}
 void Drivera::setspeed(int uspeed)
 {
 	_speed = uspeed;
 }
-void Drivera::setcoors(bool mode)
+void Drivera::setmode(bool mode)
 {
 	curmode = mode;
+}
+void Drivera::setcoors(int angle){
+	curangle = angle;
 }
 void Drivera::setsplitt(int splitt)
 {
@@ -74,6 +85,26 @@ void Drivera::setsplitt(int splitt)
 		break;
 	}
 };
-void Drivera::setangle(int angle){};
-void Drivera::linmove(int angle){};
-void Drivera::moverot(int step){};
+void Drivera::setangle(int angle){
+
+}
+void Drivera::linmove(float angle){
+float angleperstep =360/_step;
+if(!curmode){//линейное движение в относительной  системе координат
+int abscurangle = curangle-abszero;
+if(abscurangle <0 ) abscurangle = 360 + abscurangle;
+
+		int nstep = (angle - curangle) ;
+		if(nstep <0 ) nstep = 360 + nstep;
+		nstep /= angleperstep;
+		move(nstep,1);
+}
+else {//линейное движение в абсолютной системе координат
+		int nstep = (angle - curangle) / angleperstep ;
+		if(nstep <0 ) nstep = 360 + nstep;
+		nstep /= angleperstep;
+		move(nstep,1);
+}
+}
+
+void Drivera::moverot(int step){}
